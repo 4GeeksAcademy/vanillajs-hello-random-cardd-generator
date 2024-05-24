4de4945ec7b5387198e61457361a3feb155b52b4
chaolinespo <162933423+chaolinespo@users.noreply.github.com>
2024-05-24 15:01:49 +0000

    style

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const generateRandomCard = () => {
+  let values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "j",
+    "q",
+    "k",
+    "a"
+  ];
+  let suit = ["♠", "♥", "♦", "♣"];
+  let randomValues = values[Math.floor(Math.random() * values.length)];
+  let randomSuit = suit[Math.floor(Math.random() * suit.length)];
+  if (randomSuit === "♥" || randomSuit === "♦") {
+    document.querySelector(".top").style.color = "red";
+    document.querySelector(".bottom").style.color = "red";
+  } else {
+    document.querySelector(".top").style.color = "black";
+    document.querySelector(".bottom").style.color = "black";
+  }
+  document.querySelector(".top").innerHTML = randomSuit;
+  document.querySelector(".number").innerHTML = randomValues;
+  document.querySelector(".bottom").innerHTML = randomSuit;
+};
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".btn").addEventListener("click", function() {
+    generateRandomCard();
+  });
 };
